4d266b74009aeefb5ccb14f2d9e7789190ebb150
Bishwas Bhandari <bishwasbh@gmail.com>
2023-10-25 23:43:57 +0545

    custom utility button docs added

--- src/item/codes/utility-buttons/AppendUtilities.svelte
+++ src/item/codes/utility-buttons/AppendUtilities.svelte
@@ -1,12 +1,13 @@
-<!-- Use Utility.svelte slot -->
-<pre><code><span class="text-blue-400">import</span> <span class="text-green-100">{`{utilityButtonsComponent, Utility}`}</span> <span class="text-blue-400">from</span> <span class="text-green-400">"@friendofsvelte/tipex"</span>;
+
+<pre class="mt-3"><code><span class="text-blue-400">import</span> <span class="text-green-100">CustomUtilityButtons</span> <span class="text-blue-400">from</span> <span class="text-green-400">"path-to/UtilityButtons.svelte"</span>;
+<span class="text-blue-400">import</span> {'{utilityButtonsComponent}'} <span class="text-blue-400">from</span> <span class="text-green-400">"@friendofsvelte/tipex"</span>;
 
 <span class="text-gray-300">// Insert this to utilityButtonsComponent using: </span>
-{`if ($utilityButtonsComponent) {
-    new Utility({
+    {`if ($utilityButtonsComponent) {
+    new CustomUtilityButtons({
         target: $utilityButtonsComponent,
         props: {
-        }
+        } // ...props here
     })
 }`}
 </code></pre>
--- src/item/codes/utility-buttons/InsertUtilities.svelte
+++ src/item/codes/utility-buttons/InsertUtilities.svelte
@@ -1,7 +1,14 @@
 
-<pre class="mt-3"><code><span class="text-blue-400">import</span> <span class="text-green-100">UtilityButtons</span> <span class="text-blue-400">from</span> <span class="text-green-400">"path-to/UtilityButtons.svelte"</span>;
-<span class="text-blue-400">import</span> {'{utilityButtonsComponent}'} <span class="text-blue-400">from</span> <span class="text-green-400">"@friendofsvelte/tipex"</span>;
+<!-- Use Utility.svelte slot -->
+<pre><code><span class="text-blue-400">import</span> <span class="text-green-100">{`{utilityButtonsComponent, Utility}`}</span> <span class="text-blue-400">from</span> <span class="text-green-400">"@friendofsvelte/tipex"</span>;
 
-<span class="text-gray-400">// Set the Utility component in utilityButtonsComponent</span>
-<span class="text-green-100">utilityButtonsComponent</span>.<span class="text-green-400">set</span>(<span class="text-green-100">Utility</span>);
+<span class="text-gray-300">// Insert this to utilityButtonsComponent using: </span>
+    {`if ($utilityButtonsComponent) {
+    new Utility({
+        target: $utilityButtonsComponent,
+        props: {
+        }
+    })
+}`}
 </code></pre>
+

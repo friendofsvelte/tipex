ed63519fab51f8cb02642d1c2329bb016eb600bd
Bishwas Bhandari <bishwasbh@gmail.com>
2023-12-08 17:24:45 +0545

    defaultExtensions exported

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,5 +1,6 @@
 
-import Tipex from "$lib/tipex/Tipex.svelte";
-import Utility from "$lib/tipex/Utility.svelte";
-import {tipexEditor, isLoading} from '$lib/tipex/editor_store.js';
-export {tipexEditor, isLoading, Tipex, Utility};
+import Tipex from "./tipex/Tipex.svelte";
+import Utility from "./tipex/Utility.svelte";
+import {tipexEditor, isLoading} from './tipex/editor_store.js';
+import { defaultExtensions } from "./tipex/default.js";
+export {tipexEditor, isLoading, Tipex, Utility, defaultExtensions};

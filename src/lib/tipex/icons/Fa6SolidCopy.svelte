7977a36e645a144a60ff7da51219b316bc0f1058
bishwas-py <bishwasbh@gmail.com>
2025-06-30 10:36:22 +0545

    refactor: remove unused display and occupy props from Tipex icon components for cleaner code

--- src/lib/tipex/icons/Fa6SolidCopy.svelte
+++ src/lib/tipex/icons/Fa6SolidCopy.svelte
@@ -1,25 +1,19 @@
 <script lang="ts" module>
   export interface Fa6SolidCopyProps {
-    display?: boolean;
-    occupy?: boolean;
     size?: number;
     class?: string;
   }
 </script>
 
 <script lang="ts">
-  const {display = false, occupy = true, size = 0.7, class: className = ''}: Fa6SolidCopyProps = $props();
+  const {size = 0.7, class: className = ''}: Fa6SolidCopyProps = $props();
 </script>
 
-{#if display}
-  <svg
-   xmlns="http://www.w3.org/2000/svg"
-   width="{size}em"
-   height="{size}em"
-   viewBox="0 0 448 512"
-   class="{className}">
+<svg
+ xmlns="http://www.w3.org/2000/svg"
+ width="{size}em"
+ height="{size}em"
+ viewBox="0 0 448 512"
+ class="{className}">
   <path fill="currentColor" d="M208 0h124.1C344.8 0 357 5.1 366 14.1L433.9 82c9 9 14.1 21.2 14.1 33.9V336c0 26.5-21.5 48-48 48H208c-26.5 0-48-21.5-48-48V48c0-26.5 21.5-48 48-48M48 128h80v64H64v256h192v-32h64v48c0 26.5-21.5 48-48 48H48c-26.5 0-48-21.5-48-48V176c0-26.5 21.5-48 48-48"/>
 </svg>
-{:else if occupy}
-  <div style="height: {size}em; width: {size}em;"></div>
-{/if}
--- src/lib/tipex/icons/Fa6SolidItalic.svelte
+++ src/lib/tipex/icons/Fa6SolidItalic.svelte
@@ -1,25 +1,19 @@
 <script lang="ts" module>
   export interface Fa6SolidItalicProps {
-    display?: boolean;
-    occupy?: boolean;
     size?: number;
     class?: string;
   }
 </script>
 
 <script lang="ts">
-  const {display = false, occupy = true, size = 0.7, class: className = ''}: Fa6SolidItalicProps = $props();
+  const {size = 0.7, class: className = ''}: Fa6SolidItalicProps = $props();
 </script>
 
-{#if display}
-  <svg
-   xmlns="http://www.w3.org/2000/svg"
-   width="{size}em"
-   height="{size}em"
-   viewBox="0 0 384 512"
-   class="{className}">
+<svg
+ xmlns="http://www.w3.org/2000/svg"
+ width="{size}em"
+ height="{size}em"
+ viewBox="0 0 384 512"
+ class="{className}">
   <path fill="currentColor" d="M128 64c0-17.7 14.3-32 32-32h192c17.7 0 32 14.3 32 32s-14.3 32-32 32h-58.7L160 416h64c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32h58.7L224 96h-64c-17.7 0-32-14.3-32-32"/>
 </svg>
-{:else if occupy}
-  <div style="height: {size}em; width: {size}em;"></div>
-{/if}
